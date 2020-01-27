383d7408508b9e16417f8e6e8bd8aa9dce3b57a2
Peter Hoeg <peter@hoeg.com>
2020-01-27 08:18:19 +0800

    core: disable GC during start

--- init.el
+++ init.el
@@ -15,7 +15,8 @@
 ;; Avoid garbage collection during startup.
 ;; see `SPC h . dotspacemacs-gc-cons' for more info
 (defconst emacs-start-time (current-time))
-(setq gc-cons-threshold 402653184 gc-cons-percentage 0.6)
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 0.6)
 (load (concat (file-name-directory load-file-name)
               "core/core-versions.el")
       nil (not init-file-debug))
